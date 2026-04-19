1943c760841939f7c4360d585b166ba57314cbf8
lunarash0 <eamonhwatson@icloud.com>
2026-04-19 12:36:35 +0200

    Init: Baseplate

---
+++ src/root.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ComingSoon from './routes/comingSoon'
+import './root.css'
+
+const router = createBrowserRouter([
+  { path: '/', element: <ComingSoon /> },
+])
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
