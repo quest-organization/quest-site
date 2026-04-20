1dbc4ce4b01226c09c5a519e9adfacfa04caf506
lunarash0 <eamonhwatson@icloud.com>
2026-04-20 03:46:11 +0200

    upd: error page

--- src/root.tsx
+++ src/root.tsx
@@ -1,16 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, useRouteError, isRouteErrorResponse } from 'react-router-dom'
 import ComingSoon from './routes/comingSoon'
+import ErrorPage from './routes/error'
 import Invite from './routes/bot/invite'
 import './root.css'
 
 const router = createBrowserRouter([
-  { path: '/', element: <ComingSoon /> },
-  { path: '/bot/discord', element: <Invite/> }
+  {
+    errorElement: <ErrorPage />,
+    children: [
+      { path: '/', element: <ComingSoon /> },
+      { path: '/bot/invite', element: <Invite /> },
+      {
+        path: '*',
+        loader: () => {
+          throw new Response('Not Found', { status: 404, statusText: 'Not Found' })
+        },
+      },
+    ],
+  },
 ])
 
-createRoot(document.getElementById('root')!).render(
+  createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>,
