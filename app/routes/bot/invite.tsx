8e5795f61ea43a412d1728215034e813d1d0f2fd
lunarash0 <eamonhwatson@icloud.com>
2026-05-01 00:58:22 +0200

    upd: add admin to install permission

--- app/routes/bot/invite.tsx
+++ app/routes/bot/invite.tsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 export default function Invite() {
     useEffect(() => {
         window.location.replace(
-            'https://discord.com/oauth2/authorize?client_id=1494686224508522579&permissions=4504974218751046&scope=bot%20applications.commands'
+            'https://discord.com/oauth2/authorize?client_id=1494686224508522579&permissions=4504974218751054&scope=bot%20applications.commands'
         );
     }, []);
     
