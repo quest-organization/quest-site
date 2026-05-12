6b733211f5fe70cbcd4fa9f81648df2a5dc6af08
unbravechimp <eamonhwatson@icloud.com>
2026-05-12 14:52:26 +0200

    fix?

--- app/routes.ts
+++ app/routes.ts
@@ -1,4 +1,4 @@
-import { type RouteConfig, index, route, layout, prefix } from "@react-router/dev/routes";
+import { type RouteConfig, index, route, prefix } from "@react-router/dev/routes";
 
 export default [
     index("routes/home.tsx"),
