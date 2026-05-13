62b688ec526403ad5ddee74e398d3131600ac6e5
UnbraveChimp <unbravechimp@minerlegacy.net>
2026-05-13 16:34:03 +0200

    fix: why did I forget this :sob:

--- app/routes/landing/comingSoon.tsx
+++ app/routes/landing/comingSoon.tsx
@@ -1,9 +1,11 @@
 import './comingSoon.css'
+import logo from './logoBanner.svg'
+
 export function ComingSoon() {
   return (
     <div className="relative flex flex-col h-screen overflow-hidden bg-neutral-100">
       <div className="relative z-10 flex flex-col items-center justify-center h-full gap-4">
-        <img src="./logoBanner.svg" alt="Logo" />
+        <img src={logo} alt="Logo" />
       </div>
 
       <h3 className="subtext absolute bottom-24 left-0 right-0 z-10 text-center opacity-50">
